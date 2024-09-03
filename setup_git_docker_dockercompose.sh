65c4a60af5d028810603af2650c497534ff3c6ce
panda8699 <128176079+panda8699@users.noreply.github.com>
2024-09-04 01:01:46 +0700

    Update setup_git_docker_dockercompose.sh

--- setup_git_docker_dockercompose.sh
+++ setup_git_docker_dockercompose.sh
@@ -11,39 +11,6 @@ NC='\033[0m' # Không màu
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-# Cài đặt Zsh
-sudo apt-get install zsh -y
-
-# Kiểm tra phiên bản Zsh
-zsh --version
-
-# Tự động thay đổi shell mặc định sang Zsh (tránh prompt người dùng)
-chsh -s $(which zsh) $(whoami)
-
-# Cài đặt Oh My Zsh thông qua script chính thức
-RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Cài đặt plugin zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-
-# Cấu hình theme robbyrussell và thêm plugin zsh-autosuggestions vào .zshrc
-sed -i 's/ZSH_THEME=".*"/ZSH_THEME="robbyrussell"/' ~/.zshrc
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' ~/.zshrc
-
-# Áp dụng các thay đổi bằng cách tải lại .zshrc
-source ~/.zshrc
-
-# Xác nhận shell mặc định đã được thay đổi thành Zsh
-current_shell=$(echo $SHELL)
-if [[ $current_shell == *"zsh"* ]]; then
-    echo -e "${GREEN}Zsh đã được đặt làm shell mặc định thành công!${NC}"
-else
-    echo -e "${RED}Lỗi: Zsh không phải là shell mặc định. Shell hiện tại: $current_shell${NC}"
-fi
-
-echo -e "${BLUE}Cài đặt Zsh, Oh My Zsh, theme robbyrussell và plugin zsh-autosuggestions đã hoàn tất!${NC}"
-echo -e "${YELLOW}Đang chuyển sang sử dụng Zsh làm shell mặc định...${NC}"
-echo -e "${YELLOW}Vui lòng đăng xuất và đăng nhập lại để áp dụng các thay đổi.${NC}"
 
 # Cài đặt Docker
 echo -e "${BLUE}Cài đặt Docker...${NC}"
